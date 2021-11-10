64c209fdcccd24ed45ea1d66aaca9825610a0b15
Jora-h <85570244+Jora-h@users.noreply.github.com>
2021-11-10 22:29:51 +0100

    delete task

--- javascript.js
+++ javascript.js
@@ -1,19 +1,33 @@
 /*Här skriver ni koden*/
-
+// declaration of variables
 const button = document.getElementById('my-button');
 const list = document.getElementById('list');
 const input = document.getElementById('task');
 
-button.addEventListener("click", function(){
+// declaration of click handlers
+const handelAdd = () => {
     const liElement = document.createElement('li');
-    if(input.value == ""){
+    const deleteButton = document.createElement('button');
+    deleteButton.innerHTML = "delete";
+    const handelDelete = () => {
+        liElement.remove();
+        
+        // alternative
+        // list.removeChild(liElement);
+    }
+    deleteButton.addEventListener("click", handelDelete);
+    if (input.value == "") {
         alert('Write something here');
     } else {
         liElement.innerHTML = input.value;
+        liElement.appendChild(deleteButton);
         input.value = "";
         list.appendChild(liElement);
     }
-});
+}
+
+// click event binding
+button.addEventListener("click", handelAdd);
 
 
 
